5f18049147334147d1a5b5ebc3d8646f6a730573
RoAAlaniz <rocioabril.alaniz@estudiantes.unahur.edu.ar>
2026-06-04 23:21:04 -0300

    diseño Contacto y Formulario de compra

--- ecommerce/src/pages/Contacto.jsx
+++ ecommerce/src/pages/Contacto.jsx
@@ -1,5 +1,46 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import miLogo from "/src/assets/logo.png";
+import publicidadImg from "/src/assets/newProduct.png";
+
 function Contacto() {
-  return <h1>Contacto</h1>;
+  return (
+    <div className="contacto-page-container">
+      <Container className="contacto-container">
+        <Row className="align-items-center target-row">
+          <Col xs={12} md={6} className="text-section">
+            <h1 className="soporte-title">Soporte</h1>
+            <p className="soporte-description">
+              En nuestro espacio de soporte estamos comprometidos con brindarte la mejor experiencia. Ponete con nosotros, nos encantaría saber de vos, escuchar tus sugerencias o asistirte ante cualquier eventualidad con nuestra plataforma.
+            </p>
+            <Button 
+              href="mailto:roaalanizz@gmail.com" 
+              className="btn-soporte-secondary">
+              Contáctanos
+            </Button>
+          </Col>
+          
+          <Col xs={12} md={6} className="image-section">
+            <img 
+              src={miLogo} 
+              alt="Ilustración de soporte" 
+              className="img-fluid soporte-ilustration"
+            />
+          </Col>
+        </Row>
+
+        <Row className="publicidad-row">
+          <Col xs={12} lg={10} className="publicidad-col">
+            <img 
+              src={publicidadImg} 
+              alt="Publicidad Línea Apex Pro" 
+              className="publicidad-banner"
+            />
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
+  );
 }
 
 export default Contacto;
