27bee3527d56ac3083463477e4910dae7878af76
toralessantiago <toralessantiago885@gmail.com>
2026-06-10 11:17:58 -0300

    cambio a BrowserRouter

--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,9 +10,9 @@ import { CartProvider } from "./context/CarritoContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
      <CartProvider>
-      <HashRouter>
+      <BrowserRouter >
         <App />
-      </HashRouter>
+      </BrowserRouter >
     </CartProvider>
   </React.StrictMode>
 );
