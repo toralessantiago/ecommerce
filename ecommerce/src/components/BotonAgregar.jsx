433d22fde885bfafbb2d04f9a784a1035532bb33
Martina Movia <moviamartina@hotmail.com>
2026-06-10 22:40:23 -0300

    Mensaje de exito

--- ecommerce/src/components/BotonAgregar.jsx
+++ ecommerce/src/components/BotonAgregar.jsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CarritoContext } from "../context/CarritoContext";
+import { Button, Toast, ToastContainer } from "react-bootstrap";
 
 function BotonAgregar({
   producto,
   talle,
   cantidadEnCarrito = 0,
-  setErrorTalle = () => {},
+  setErrorTalle = () => { },
 }) {
   const { agregarAlCarrito } = useContext(CarritoContext);
+
+  const [showToast, setShowToast] = useState(false);
 
   const handleClick = () => {
     if (producto.talles?.length > 0 && !talle) {
@@ -19,21 +22,44 @@ function BotonAgregar({
       ...producto,
       talle,
     });
+
+    setShowToast(true);
+
   };
 
   return (
-    <button
-      className="btn btn-dark w-100 mt-2"
-      disabled={
-        producto.stock === 0 ||
-        cantidadEnCarrito >= producto.stock
-      }
-      onClick={handleClick}
-    >
-      {cantidadEnCarrito >= producto.stock
-        ? "Sin stock"
-        : "Agregar al carrito"}
-    </button>
+    <>
+      <Button
+        className="btn btn-dark w-100 mt-2"
+        disabled={
+          producto.stock === 0 ||
+          cantidadEnCarrito >= producto.stock
+        }
+        onClick={handleClick}
+      >
+        {cantidadEnCarrito >= producto.stock
+          ? "Sin stock"
+          : "Agregar al carrito"}
+      </Button>
+
+      <ToastContainer 
+        position="bottom-end" 
+        className="p-3" 
+        style={{ position: 'fixed', zIndex: 1050 }}
+      >
+        <Toast 
+          onClose={() => setShowToast(false)} 
+          show={showToast} 
+          delay={3000} 
+          autohide
+          bg="success" 
+        >
+          <Toast.Body className="text-white fw-bold">
+            ¡Agregado al carrito exitosamente!
+          </Toast.Body>
+        </Toast>
+        </ToastContainer>
+    </>
   );
 }
 
